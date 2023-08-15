a3af05981b769265d0dd955ab8c5d47041e398b9
yukseltron <h.yuksel04@gmail.com>
2023-08-15 18:10:04 -0400

    add readme to example and fix example site bug

--- example/main.js
+++ example/main.js
@@ -6,11 +6,11 @@ const element2 = document.querySelector('#headline2');
 const element3 = document.querySelector('#headline3');
 
 const fx1 = new TextFX.Scrambler();
-const fx2 = new TextFX.Randomizer();
-const fx3 = new TextFX.Redacter();
+const fx2 = new TextFX.Shuffler();
+const fx3 = new TextFX.Replacer();
 
 
 
 fx1.scramble(element1, 100, 10, true);
-fx2.randomize(element2, 200, 10, true);
-fx3.censor(element3, 100, "█", true);
+fx2.shuffle(element2, 200, 10, true);
+fx3.replace(element3, 100, "█", true);
